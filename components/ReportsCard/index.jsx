20b30af6924dcf28fb780e82092a4c401fbbf9a5
irinainina <irbris@mail.ru>
2022-07-13 07:21:48 +0300

    feat: add download pdf file

--- components/ReportsCard/index.jsx
+++ components/ReportsCard/index.jsx
@@ -6,7 +6,24 @@ import getFile from '../../utils/getFile';
 const ReportsCard = ({ data }) => {
   const {reportCountry, reportFile, reportImage, reportTitle, reportSubTitle, reportRegion} = data;
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
+    const url = getFile(reportFile);
+    await fetch(url, {
+      method: 'GET'
+  }).then(resp => resp.blob())
+      .then(blob => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = url;
+          a.download = "report.pdf";
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+      });
+  };
 
   return (
     <div className="reports-card">
@@ -14,11 +31,8 @@ const ReportsCard = ({ data }) => {
         <Image className="reports-card__imgbox__img" src={getImg(reportImage)} alt="img" layout="fill" priority />
       </div>
       <h3 className="reports-card__title">{reportTitle}</h3>
-      <p className="reports-card__subtitle">{reportSubTitle}</p>      
-      <a href={getFile(reportFile)} download="report.pdf" className="reports-card__link">
-        <span className="reports-card__link__span">Download</span>
-        <Image src={arrowDown} alt="download" />
-      </a>   
+      <p className="reports-card__subtitle">{reportSubTitle}</p>
+      <form onSubmit={onSubmit}><input type="submit" value="Download" className="reports-card__download" /></form>
     </div>
   );
 };
