51ba0c67f3a5ca7d422cd8450bfe7a8ebdcc5abc
irinainina <irbris@mail.ru>
2022-07-19 18:48:35 +0300

    feat: add adaptive to involved page

--- components/sections/InvolvedForm/index.jsx
+++ components/sections/InvolvedForm/index.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import getImg from '../../../utils/getImg'
+import getImg from '../../../utils/getImg';
+import arrowRight from '../../../assets/images/arrowRight.svg';
 
 const InvolvedForm = ({data}) => {
   const {involvedFormImage, involvedFormTitle} = data;
@@ -9,51 +10,22 @@ const InvolvedForm = ({data}) => {
       <div className="ifktion__center">
         <h2 className="ifktion__title">{involvedFormTitle}</h2>
         <form className="ifktion__form">
-          <input type="text" placeholder="Name *"className="ifktion__input ifktion__half--input"/>
-          <input type="text" placeholder="Last Name *"className="ifktion__input ifktion__half--input"/>
-          <input type="email" placeholder="E-mail *"className="ifktion__input"/>
-          <input type="text" placeholder="Company *"className="ifktion__input ifktion__half--input"/>
-          <input type="text" placeholder="Job title *"className="ifktion__input ifktion__half--input"/> 
+          <input type="text" placeholder="First Name *"className="ifktion__input ifktion__half--input" name="firstname" required/>
+          <input type="text" placeholder="Last Name *"className="ifktion__input ifktion__half--input" name="lastname" required/>
+          <input type="text" placeholder="Company *"className="ifktion__input" name="company" required/>
+          <input type="email" placeholder="E-mail *"className="ifktion__input" name="email" required/>     
           <label htmlFor="group" className="ifktion__form-subtitle">Which group do you best represent?</label>
-          <select name="group" id="group" className="ifktion__input ifktion__select">
+          <select name="0-2/which_group_do_you_best_represent_" id="group" className="ifktion__select">
             <option value="large">Large Corporation ($1 billion + in revenue)</option>
             <option value="medium">Medium Corporation ($10million - $1billion)</option>
             <option value="small">Small Corporation ($10million or less)</option>
             <option value="largeNGO">Large NGO (100+ employees)</option>
             <option value="smallNGO">Small NGO (less than 100 employees)</option>
             <option value="government">Government & Academia</option>
-          </select>
-          <h3 className="ifktion__form-subtitle">How did you hoar about us?</h3>
-          <div className="ifktion__checkbox-wide-wrapper">
-            <label className="ifktion__label">
-              <input type="checkbox" name="info" value="network" className="ifktion__checkbox" />
-              Ocean Plastics Leadership Network
-            </label>
-            <label className="ifktion__label">
-              <input type="checkbox" name="info" value="facebook" className="ifktion__checkbox"/>
-              Facebook
-            </label>
-            <label className="ifktion__label">
-              <input type="checkbox" name="info" value="linkedin" className="ifktion__checkbox"/>
-              Linkedin
-            </label>
-          </div>
-          <div className="ifktion__checkbox-wrapper">
-            <label className="ifktion__label">
-              <input type="checkbox" name="info" value="news" className="ifktion__checkbox"/>
-              News
-            </label>
-            <label className="ifktion__label">
-              <input type="checkbox" name="info" value="twitter" className="ifktion__checkbox"/>
-              Twitter
-            </label>
-            <label className="ifktion__label">
-              <input type="checkbox" name="info" value="other" className="ifktion__checkbox"/>
-              Other
-            </label>
-          </div>
+          </select>          
           <button type="submit" className="ifktion__submit">
-            Submit          
+            Submit
+            <Image src={arrowRight} alt="black arrow" />
           </button>
         </form>
       </div>
