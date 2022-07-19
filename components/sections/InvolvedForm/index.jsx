e1f85827487b38cbe87b294101b3a00eaf86a8fe
irinainina <irbris@mail.ru>
2022-07-19 13:06:05 +0300

    feat: add style to form

--- components/sections/InvolvedForm/index.jsx
+++ components/sections/InvolvedForm/index.jsx
@@ -9,44 +9,46 @@ const InvolvedForm = ({data}) => {
       <div className="ifktion__center">
         <h2 className="ifktion__title">{involvedFormTitle}</h2>
         <form className="ifktion__form">
-          <input type="text" placeholder="Name"className="ifktion__input ifktion__half--input"/>
-          <input type="text" placeholder="Last Name"className="ifktion__input ifktion__half--input"/>
-          <input type="email" placeholder="E-mail"className="ifktion__input"/>
-          <input type="text" placeholder="Company"className="ifktion__input ifktion__half--input"/>
-          <input type="text" placeholder="Job title"className="ifktion__input ifktion__half--input"/> 
-          <label htmlFor="group" className="ifktion__label">Which group do you best represent?</label>
-          <select name="group" id="group">
+          <input type="text" placeholder="Name *"className="ifktion__input ifktion__half--input"/>
+          <input type="text" placeholder="Last Name *"className="ifktion__input ifktion__half--input"/>
+          <input type="email" placeholder="E-mail *"className="ifktion__input"/>
+          <input type="text" placeholder="Company *"className="ifktion__input ifktion__half--input"/>
+          <input type="text" placeholder="Job title *"className="ifktion__input ifktion__half--input"/> 
+          <label htmlFor="group" className="ifktion__form-subtitle">Which group do you best represent?</label>
+          <select name="group" id="group" className="ifktion__input ifktion__select">
             <option value="large">Large Corporation ($1 billion + in revenue)</option>
-            <option value="medium">Medium Corporation ($100 000 + in revenue)</option>
-            <option value="small">Small Corporation ($10 000 + in revenue)</option>
-            <option value="person">Individual person</option>
+            <option value="medium">Medium Corporation ($10million - $1billion)</option>
+            <option value="small">Small Corporation ($10million or less)</option>
+            <option value="largeNGO">Large NGO (100+ employees)</option>
+            <option value="smallNGO">Small NGO (less than 100 employees)</option>
+            <option value="government">Government & Academia</option>
           </select>
-          <h3 className="ifktion__checkbox-title">How did you hoar about us?</h3>
-          <div className="ifktion__checkbox-wrapper">
-            <label>
-              <input type="checkbox" name="info" value="network" />
+          <h3 className="ifktion__form-subtitle">How did you hoar about us?</h3>
+          <div className="ifktion__checkbox-wide-wrapper">
+            <label className="ifktion__label">
+              <input type="checkbox" name="info" value="network" className="ifktion__checkbox" />
               Ocean Plastics Leadership Network
             </label>
-            <label>
-              <input type="checkbox" name="info" value="facebook" />
+            <label className="ifktion__label">
+              <input type="checkbox" name="info" value="facebook" className="ifktion__checkbox"/>
               Facebook
             </label>
-            <label>
-              <input type="checkbox" name="info" value="linkedin" />
+            <label className="ifktion__label">
+              <input type="checkbox" name="info" value="linkedin" className="ifktion__checkbox"/>
               Linkedin
             </label>
           </div>
           <div className="ifktion__checkbox-wrapper">
-            <label>
-              <input type="checkbox" name="info" value="news" />
+            <label className="ifktion__label">
+              <input type="checkbox" name="info" value="news" className="ifktion__checkbox"/>
               News
             </label>
-            <label>
-              <input type="checkbox" name="info" value="twitter" />
+            <label className="ifktion__label">
+              <input type="checkbox" name="info" value="twitter" className="ifktion__checkbox"/>
               Twitter
             </label>
-            <label>
-              <input type="checkbox" name="info" value="other" />
+            <label className="ifktion__label">
+              <input type="checkbox" name="info" value="other" className="ifktion__checkbox"/>
               Other
             </label>
           </div>
