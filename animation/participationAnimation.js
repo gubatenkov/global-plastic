535ba9432f1061d844feb82825daabf89b3cf22a
irinainina <irbris@mail.ru>
2022-07-21 16:51:58 +0300

    feat: update animation

--- animation/participationAnimation.js
+++ animation/participationAnimation.js
@@ -2,19 +2,13 @@ const participationAnimation = (percent, icon) => {
   const number = icon.current.firstElementChild;   
   if(percent > -40 && percent < -14) {
     icon.current.style.transform = 'scale(1)';
+    number.style.display = 'block';
+  } else if(percent > -14 && percent < 95) {
+    icon.current.style.transform = `scale(${1 + (percent + 14) / 100})`;
     icon.current.style.opacity = '1';
     number.style.display = 'block';
-  } else if(percent > -14 && percent < 60) {
-    icon.current.style.transform = `scale(${1 + (percent + 14) / 50})`;
-    icon.current.style.opacity = '1';
-    number.style.display = 'block';
-  } else if(percent > 60 && percent < 95) {
-    icon.current.style.transform = 'scale(20)';
-    icon.current.style.opacity = '.8';
-    number.style.display = 'none';
   } else if(percent < -40 || percent > 95) {
-    icon.current.style.transform = 'scale(.1)';
-    icon.current.style.opacity = '1';
+    icon.current.style.transform = 'scale(.25)';
     number.style.display = 'block';
   }       
 }
