241a6a3349484e660a3e71355e0e42878e305c81
irinainina <irbris@mail.ru>
2022-07-21 15:03:28 +0300

    feat: update animation

--- animation/participationAnimation.js
+++ animation/participationAnimation.js
@@ -1,14 +1,18 @@
 const participationAnimation = (percent, icon) => {
   const number = icon.current.firstElementChild;   
-  if(percent > -30 && percent < 10) {
+  if(percent > -40 && percent < -14) {
     icon.current.style.transform = 'scale(1)';
     icon.current.style.opacity = '1';
     number.style.display = 'block';
-  } else if(percent > 50 && percent < 85) {
-    icon.current.style.transform = 'scale(25)';
-    icon.current.style.opacity = '.5';
+  } else if(percent > -14 && percent < 60) {
+    icon.current.style.transform = `scale(${1 + (percent + 14) / 50})`;
+    icon.current.style.opacity = '1';
+    number.style.display = 'block';
+  } else if(percent > 60 && percent < 95) {
+    icon.current.style.transform = 'scale(20)';
+    icon.current.style.opacity = '.8';
     number.style.display = 'none';
-  } else if(percent < -30 || percent > 85) {
+  } else if(percent < -40 || percent > 95) {
     icon.current.style.transform = 'scale(.1)';
     icon.current.style.opacity = '1';
     number.style.display = 'block';
