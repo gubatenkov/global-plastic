521973698e03146c1d24d2661968e2fe6bec9f60
irinainina <irbris@mail.ru>
2022-07-22 13:02:32 +0300

    feat: minor edits

--- animation/involvedAnimation.js
+++ animation/involvedAnimation.js
@@ -1,5 +1,5 @@
 const involvedAnimation = (percent, icon) => {      
-  if(percent < 5 || percent > 112) {
+  if(percent <= 5 || percent > 90) {
     icon.current.firstElementChild.style.display = 'block';
     icon.current.style.transform = `scale(1)`;
     icon.current.style.top = `110px`;
@@ -14,7 +14,7 @@ const involvedAnimation = (percent, icon) => {
     icon.current.style.transform = `scale(${percent/1.2}) translateX(4%)`;
     icon.current.style.top = 110 + percent * 4 + 'px';
     icon.current.style.maxHeight = `30px`;
-  } else if(percent > 75 && percent <= 112) {
+  } else if(percent > 75 && percent <= 90) {
     icon.current.firstElementChild.style.display = 'none';
     icon.current.style.transform = `scale(${percent/1.2}) translateX(4%)`;
     icon.current.style.top = 110 + percent * 4 + 'px';
