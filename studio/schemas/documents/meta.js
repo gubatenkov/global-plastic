be713839a75e32451b39bc02835a2b99608a03c9
irinainina <irbris@mail.ru>
2022-07-25 19:25:16 +0300

    feat: add sitemap, meta, robots.txt

---
+++ studio/schemas/documents/meta.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'meta',
+  title: 'Meta',
+  type: 'document',
+  fields: [
+    {
+      name: 'ogTitle',
+      title: 'Og Title',      
+      type: 'string',
+    },
+    {
+      name: 'ogDescription',
+      title: 'Og Description',      
+      type: 'string',
+    },
+    {
+      name: 'twitterSite',
+      title: 'Twitter Site',      
+      type: 'string',
+    },
+    {
+      name: 'type',
+      title: 'Type',      
+      type: 'string',
+    },
+    {
+      name: 'url',
+      title: 'URL',      
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',      
+      type: 'image',
+    },    
+  ],
+}
