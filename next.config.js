353fa0a5db72ed94b940fee82b67a494fa0b359b
irinainina <irbris@mail.ru>
2022-07-26 11:15:58 +0300

    feat: update sitemap

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@
 module.exports = {
   images: {
     domains: ['cdn.sanity.io', 'img.youtube.com']
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/sitemap-generator");
+    }
+    return config;
+  },
 };
