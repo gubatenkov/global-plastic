be998de79194070411e193b1fffe0821e39df944
irinainina <irbris@mail.ru>
2022-07-28 13:07:32 +0300

    feat: add roadmapHero section

--- components/RoadmapHeroCard/index.jsx
+++ components/RoadmapHeroCard/index.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import arrowRight from 'assets/images/arrowRight.svg';
+import arrowRight from '../../assets/images/arrowRight.svg';
 import pin from '../../assets/images/pin.svg';
 import getImg from '../../utils/getImg';
 
@@ -8,32 +8,32 @@ const RoadmapHeroCard = ({ data }) => {
   const {roadmapHeroCountry, roadmapHeroRegion, roadmapHeroDate, roadmapHeroTime, roadmapHeroTitle, roadmapHeroDescription, roadmapHeroLinkName, roadmapHeroLinkURL, roadmapHeroImage } = data;
 
   return (
-    <div className="roadmap-hero-card">
-      <div className="roadmap-hero-card__imgbox">
-        <Image className="roadmap-hero-card__imgbox__img" src={getImg(roadmapHeroImage)} alt="img" layout="fill" priority />
-      </div>
+    <div className="roadmap-hero-card" style={{backgroundImage: `url('${getImg(roadmapHeroImage)}')`}}>
+      <div className="roadmap-hero-card__bg">
       <div className="roadmap-hero-card__content">
         <div className="roadmap-hero-card__text">
-          <div className="roadmap-hero-card__location">
-            <Image src={pin} alt="pin" />
-            <span className="roadmap-hero-card__location__span">{roadmapHeroCountry}</span>
-            <span className="roadmap-hero-card__location__span">{roadmapHeroRegion}</span>
-          </div>
-          <h3 className="roadmap-hero-card__activation-title">
-            <span>{roadmapHeroTitle}</span>
-            <span>{roadmapHeroDescription}</span>
-          </h3>
-          <p className="roadmap-hero-card__date">
-            <span className="roadmap-hero-card__date--day">{roadmapHeroDate}</span>
-            <span>{roadmapHeroTime}</span>
-          </p>
-        </div>
-        <Link  href={roadmapHeroLinkURL}>
-          <a className="roadmap-hero-card__link modified-link arrow-hover-right">
-            <span className="roadmap-hero-card__link__span">{roadmapHeroLinkName}</span>
-            <Image src={arrowRight} alt="black arrow" className="arrow-image" />
-          </a>
-        </Link>        
+          <div className="roadmap-hero-card__header">
+            <div className="roadmap-hero-card__location">
+              <Image src={pin} alt="pin" />
+              <span className="roadmap-hero-card__location__country">{roadmapHeroCountry}</span>
+              <span className="roadmap-hero-card__dot">•</span>
+              <span className="roadmap-hero-card__location__region">{roadmapHeroRegion}</span>
+            </div>
+            <p className="roadmap-hero-card__date">
+              <span className="roadmap-hero-card__date--day">{roadmapHeroDate}</span>
+              <span>{roadmapHeroTime}</span>
+            </p>
+          </div>          
+          <h3 className="roadmap-hero-card__title">{roadmapHeroTitle}</h3>
+          <p className="roadmap-hero-card__description">{roadmapHeroDescription}</p>
+          <Link  href={roadmapHeroLinkURL}>
+            <a className="roadmap-hero-card__link modified-link arrow-hover-right">
+              <span className="roadmap-hero-card__link__span">{roadmapHeroLinkName}</span>
+              <Image src={arrowRight} alt="black arrow" className="roadmap-hero-card__arrow arrow-image"/>
+            </a>
+          </Link>          
+        </div>               
+      </div>
       </div>
     </div>
   );
