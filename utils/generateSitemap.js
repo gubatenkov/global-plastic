d45522c8a5f614e5c3b38cb58615cce7c8b1daa1
s9379992g <s9379992g@gmail.com>
2022-08-11 13:25:36 +0300

    .

--- utils/generateSitemap.js
+++ utils/generateSitemap.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 sitemap({
   baseUrl: 'https://fastidious-vacherin-1a0ea7.netlify.app',
-  pagesDirectory: path.resolve(__dirname, './pages'),
-  targetDirectory: path.resolve(__dirname, './'),
+  pagesDirectory: path.resolve(__dirname, '../public/pages/'),
+  targetDirectory: path.resolve(__dirname, '../public/'),
   ignoredExtensions: ['js', 'map', 'json', 'xml', 'png', 'css', 'jpeg', 'jpg', 'icon', 'webp'],
   ignoredPaths: [
     '404',
